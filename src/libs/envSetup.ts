67947c4b452c2dbe7e6aac8ec90159d3032162e3
nady4 <nadyajerochim@gmail.com>
2025-09-02 17:52:06 -0300

    /health

--- src/libs/envSetup.ts
+++ src/libs/envSetup.ts
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("SELF_URL:", process.env.SELF_URL);
+
 // Self-ping to keep the app awake on Render
 if (process.env.NODE_ENV === "production") {
   setInterval(() => {
